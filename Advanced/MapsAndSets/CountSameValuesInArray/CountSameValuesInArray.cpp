610149855be98403829ad9875b9a537d94c7d870
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-14 21:05:09 +0300

    Solved CountSameValuesInArray

--- Advanced/MapsAndSets/CountSameValuesInArray/CountSameValuesInArray.cpp
+++ Advanced/MapsAndSets/CountSameValuesInArray/CountSameValuesInArray.cpp
@@ -1,9 +1,45 @@
 // CountSameValuesInArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// 
+//Write a program that counts in a given array of double values the number of occurrences of each value. 
 
 #include <iostream>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<double> getOccurrences(string& line, unordered_map<double, int>& occurrences) {
+	istringstream iss(line);
+	double num;
+
+	vector<double> input;
+
+	while (iss >> num) {
+		if (!occurrences.count(num)) {
+			input.push_back(num);
+		}
+		occurrences[num]++;
+	}
+
+	return input;
+}
+
+void printResult(unordered_map<double, int>& occurrences, vector<double>& input) {
+	for (const auto& key : input) {
+		cout << key << " - " << occurrences[key] << " times" << endl;
+	}
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string line;
+	getline(cin, line);
+
+	unordered_map<double, int> occurrences;
+	vector<double> input = getOccurrences(line, occurrences);
+
+	printResult(occurrences, input);
 }
