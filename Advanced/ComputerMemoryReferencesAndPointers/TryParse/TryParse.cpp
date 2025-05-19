172578ef8396a041a6c1e1bccedef852b63de448
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-19 10:51:19 +0300

    Solved TryParse

--- Advanced/ComputerMemoryReferencesAndPointers/TryParse/TryParse.cpp
+++ Advanced/ComputerMemoryReferencesAndPointers/TryParse/TryParse.cpp
@@ -1,20 +1,40 @@
-// TryParse.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿// TryParse.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Write a program that:
+//•	Reads two strings, each of which is either a valid integer or contains only letters
+//•	Try to parse them into integer numbers
+//o	If both numbers can be parsed, calculate and print sum of the parsed numbers
+//o	Otherwise, two lines are printed :
+//	One of them for the first number, the other for the second number
+//	Each of those lines contains either the input string for that number, but if the number was not successfully parsed, the line starts with "[error] " followed by the input string for the number
+
 
 #include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+bool TryParseInt(string& input, int& result) {
+	stringstream ss(input);
+	ss >> result;
+
+	return ss.eof() && !ss.fail();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string input1, input2;
+	cin >> input1 >> input2;
+
+	int num1, num2;
+	bool isNum1Valid = TryParseInt(input1, num1);
+	bool isNum2Valid = TryParseInt(input2, num2);
+
+	if (isNum1Valid && isNum2Valid) {
+		cout << num1 + num2;
+	}
+	else {
+		cout << (!isNum1Valid ? "[error] " : "") << input1 << endl;
+		cout << (!isNum2Valid ? "[error] " : "") << input2 << endl;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
