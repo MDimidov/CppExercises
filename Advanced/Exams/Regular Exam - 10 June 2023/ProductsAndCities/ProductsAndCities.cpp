a2bae90bb8301a2a9e0b732fb284dd486827bb27
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-21 15:58:32 +0300

    Add Products and Cities

---
+++ Advanced/Exams/Regular Exam - 10 June 2023/ProductsAndCities/ProductsAndCities.cpp
@@ -0,0 +1,18 @@
+// ProductsAndCities.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//1.	Products and Cities
+//By given a number of Cities, price per product and quantity of products, write a program that calculates the Total price(product* pricePerProduct).The first number is the number of cities we want to input.The second is the CITY.The third is the price per product.The fourth is the quantity of products.
+//Input
+//On the first line you will receive the number of cities.
+//On the next lines you will receive the cities, followed by the price per product and quantity of products.
+//Output
+//Print the City followed by the result number.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
