91fc1cc81d3b6f48e63e23164757b86463ae20af
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-21 10:29:48 +0300

    Add Shopping List Task Description

---
+++ Advanced/Exams/Regular Exam - 10 June 2023/ShoppingList/ShoppingList.cpp
@@ -0,0 +1,25 @@
+// ShoppingList.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//1.	Shopping List
+//Help Tom go to the supermarket.He has a list of Shopping Items with a price and count of each of them.
+//Input
+//•	On the first line, you are given an integer number of Shopping items.
+//•	On the next lines, you are given :
+//o	Name of Shopping Items
+//o	Price of Item
+//o	Shopping Item’s Count
+//Note : There can only be one item with the same name.
+//Output
+//•	Print the total sum of the products – "The total sum is: {total sum} lv."
+//•	Sort the Shopping Items by their final price.
+//•	The final price is calculated by multiplying the Shopping Item’s count by the Shopping Item’s price(finalPrice = count * price)
+//Note : If there are two or more items with the same price, print them in the order of their entrance.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
