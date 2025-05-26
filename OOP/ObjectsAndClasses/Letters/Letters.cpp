01aae64ca23c8a7ba8db41225030f0858a9e87f3
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-26 13:03:19 +0300

    Solved Letters

--- OOP/ObjectsAndClasses/Letters/Letters.cpp
+++ OOP/ObjectsAndClasses/Letters/Letters.cpp
@@ -1,106 +1,117 @@
-// 1.	Letters
-//You are given a text in English.Let's define a word as any sequence of alphabetical characters. Each of those characters we will call a letter, but we will consider the uppercase and lowercase variant of a character in a word as the same letter.
-//Write a program that reads the text(a single line on the console) and then reads lines, each containing a single letter, until a line containing a '.' (dot)is entered.
-//For each of those lines, print all words that contain the letter, ordered alphabetically(capitals letters before lowercase letters), without duplicates – if no words contain that letter, print "---" (three dashes)
+// Letters.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//You are given a text in English. Let's define a word as any sequence of alphabetical characters. Each of those characters we will call a letter, but we will consider the uppercase and lowercase variant of a character in a word as the same letter.
+//Write a program which reads the text(a single line on the console) and then reads lines, each containing a single letter, until a line containing a '.' (dot)is entered.For each of those lines, print all words that contain the letter, ordered alphabetically(capitals letters before lowercase letters), without duplicates – if no words contain that letter, print "---" (three dashes)
+
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <map>
 #include <vector>
-#include <algorithm>
-#include<set>
+#include <set>
 
 using namespace std;
 
-class Sentence {
-private:
-	string sentences;
+bool isValidLetter(char& letter) {
+	if ((letter >= 'A' && letter <= 'Z') ||
+		(letter >= 'a' && letter <= 'z')) {
+		return true;
+	}
 
-	vector <string> splitedSentences;
+	return false;
+}
 
-	void GetSplitSentences() {
-		string word{ "" };
-		for (int i = 0; i < sentences.size(); i++) {
-			char currSymbol = sentences[i];
-			if (currSymbol == ' ' || currSymbol == ',' || currSymbol == '.') {
-				splitedSentences.push_back(word);
-				word.clear();
-				continue;
+set<string> getSentences(string& line) {
+	istringstream iss(line);
+	set<string> result;
+
+	string word;
+	while (iss >> word) {
+		string currWord;
+
+		for (auto& letter : word) {
+			if (isValidLetter(letter)) {
+				currWord += letter;
 			}
-
-			word += currSymbol;
-		}
-	}
-
-public:
-	Sentence(string sentences) {
-		this->sentences = sentences;
-		GetSplitSentences();
-	}
-
-	vector<set<string>> GetWords(vector<char>& letters) const {
-		set<string> choosenWords;
-		vector<set<string>> resultWords;
-
-		for (char letter : letters) {
-			for (int i = 0; i < splitedSentences.size(); i++) {
-				string currWord = splitedSentences[i];
-
-				for (int k = 0; k < currWord.size(); k++) {
-					char currSymbol = currWord[k];
-					if (toupper(currSymbol) == toupper(letter)) {
-						choosenWords.insert(currWord);
-						break;
-					}
+			else {
+				if (!currWord.empty()) {
+					result.insert(currWord);
 				}
-
-			}
-			resultWords.push_back(choosenWords);
-			choosenWords.clear();
-		}
-
-		bool isEmpty = false;
-		for (auto words : resultWords) {
-			if (words.size() < 0) {
-				isEmpty = true;
-				break;
+				currWord.clear();
 			}
 		}
 
-		if (isEmpty) {
-			resultWords.clear();
-			choosenWords.insert("---");
-			resultWords.push_back(choosenWords);
-		}
-
-		return resultWords;
+		result.insert(currWord);
 	}
-};
 
-void printResult(vector<set<string>>& results) {
-	for (auto sentence : results) {
-		for (string word : sentence) {
+	return result;
+}
+
+vector<char> getSearchingSymbolsFromConsole() {
+	char searchingSymbol;
+	cin >> searchingSymbol;
+
+	vector<char> searchingSymbols;
+	while (searchingSymbol != '.') {
+		searchingSymbols.push_back(searchingSymbol);
+		cin >> searchingSymbol;
+	}
+
+	return searchingSymbols;
+}
+
+bool isWordContainsLetter(string& word, char letter) {
+	for (int i = 0; i < word.size(); i++) {
+		if (tolower(word[i]) == tolower(letter)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+map<int, set<string>> getResultWords(set<string>& sentences, vector<char>& searchingSymbols) {
+	map<int, set<string>> resultWords;
+	int key = 1;
+	for (auto& symbol : searchingSymbols) {
+		bool isLetterContained = false;
+		for (auto word : sentences) {
+			if (isWordContainsLetter(word, symbol)) {
+				resultWords[key].insert(word);
+				isLetterContained = true;
+			}
+		}
+
+		if (!isLetterContained) {
+			resultWords[key].insert("---");
+		}
+
+		key++;
+	}
+
+	return resultWords;
+}
+
+void printResultWords(map<int, set<string>> resultWords) {
+	for (auto& kvp : resultWords) {
+		for (auto& word : kvp.second) {
 			cout << word << " ";
 		}
 		cout << endl;
 	}
 }
+
+
 int main()
 {
-	string sentence;
-	getline(cin, sentence);
-	Sentence sentenceClass(sentence);
+	string line;
+	getline(cin, line);
 
-	vector<char> letters;
-	while (true) {
-		char letter;
-		cin >> letter;
-		if (letter == '.') {
-			break;
-		}
+	set<string> sentences = getSentences(line);
 
-		letters.push_back(letter);
-	}
+	vector<char> searchingSymbols = getSearchingSymbolsFromConsole();
 
-	vector<set<string>> result = sentenceClass.GetWords(letters);
-	printResult(result);
+	map<int, set<string>> resultWords = getResultWords(sentences, searchingSymbols);
+
+	printResultWords(resultWords);
 }
