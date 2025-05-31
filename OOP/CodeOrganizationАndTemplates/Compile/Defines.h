9a2fd87e0c1b42fb705107a7923104cc4f976693
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-31 13:16:10 +0300

    Solved Compile

---
+++ OOP/CodeOrganizationАndTemplates/Compile/Defines.h
@@ -0,0 +1,16 @@
+//Compile
+//You are given some code, which reads a single string from the input and writes it to the output without any changes.
+//The code however doesn’t compile.Figure out what file(s) needs to be created, and what file it needs to contain, so that the program compiles and runs as described.
+//You should submit a single.zip file for this task, containing ONLY the file(s) YOU created.The Judge system has a copy of the other files and will compile them, along with your file, in the same directory.
+
+
+#pragma once
+#ifndef DEFINES_H
+#define DEFINES_H
+#define DONT_COMPILE_THIS
+#define STANDARD_TEMPLATE_LIBRARY namespace std;
+
+#include<iostream>
+#include<string>
+
+#endif // !DEFINES_H
