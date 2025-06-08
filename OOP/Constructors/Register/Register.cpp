253e1662be52358baf313a873010d5cff31138a3
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-06-08 20:12:00 +0300

    Solved Register

---
+++ OOP/Constructors/Register/Register.cpp
@@ -0,0 +1,34 @@
+//Register
+//You are given code that reads information about Company objects from the console, parses it multiple times(the number of repetitions is entered on the first line on the console), and prints the information about one of the Company objects, specified by its id.
+//The provided code handles input, output, and the repeated executions – your task is to implement the Register(which is declared in the Register.h file, you need to create the Register.cpp file) class it uses for storing and looking up the Company objects.
+//You should submit a single.zip file for this task, containing ONLY the file(s) YOU created.The Judge system has a copy of the other files and will compile them, along with your file, in the same directory.
+//Restrictions
+//There will always be a Company with the specified id.
+//Make sure there are no memory leaks.
+
+#include "Register.h"
+
+Register::Register(size_t numCompanies)
+{
+	numAdded = 0;
+	companiesArray = new Company[numCompanies];
+}
+
+void Register::add(const Company& c) {
+	companiesArray[numAdded++] = c;
+}
+
+Company Register::get(int companyId) const {
+	for (int i = 0; i < numAdded; i++) {
+		if (companiesArray[i].getId() == companyId) {
+			return companiesArray[i];
+		}
+	}
+
+	return Company();
+}
+
+Register::~Register()
+{
+	delete[] companiesArray;
+}
